6b3f01a74ce46e06a65f2f6226ec5576eea1d23b
Mathieu Lala <mathieu.lala@epitech.eu>
2021-06-03 00:23:30 +0900

    refact: clean a bit engine (#20)

--- src/KawaiiEngine/include/resources/Texture.hpp
+++ src/KawaiiEngine/include/resources/Texture.hpp
@@ -1,19 +1,10 @@
 #pragma once
 
 namespace kawe {
-    struct Texture {
-
-        Texture(int w, int h, int c, unsigned char *d)
-            : width    { w }
-            , height   { h }
-            , channels { c }
-            , data     { d } {}
-
-        ~Texture() = default;
-
-        int width;
-        int height;
-        int channels;
-        unsigned char *data;
-    };
-}
+struct Texture {
+    int width;
+    int height;
+    int channels;
+    unsigned char *data;
+};
+} // namespace kawe
